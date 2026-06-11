55340d0442a67ddefce519d345b82ffebf4f3df1
evellyn-create <evellynribeirosanto@gmail.com>
2026-06-11 13:18:57 +0000

    estilizando protótipo

--- app.py
+++ app.py
@@ -3,5 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index ():
+def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/faleconosco')
+def faleconosco():
+    return render_template('faleconosco.html')
+
+@app.route('/gestaodedescarte')
+def gestaodedescarte():
+    return render_template('gestaodedescarte.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
